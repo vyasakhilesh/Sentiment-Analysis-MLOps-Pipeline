da5c9030339ebb7fe86d3ff30c4e813c99d56f5b
vyasakhilesh <akh.vyas@gmail.com>
2024-09-30 21:31:35 +0200

    module path

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 
 
 # Database setup
-# DATABASE_URL = "postgresql://user:password@localhost/sentiment_db"
+# DATABASE_URL = "postgresql+psycopg2://user:password@localhost/sentiment_db"
 database_url = os.environ.get('DATABASE_URL')
 engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
