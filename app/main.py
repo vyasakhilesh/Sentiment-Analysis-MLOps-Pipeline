ad589ebde3d226a50eca8c7bafe0efc211d0cf65
vyasakhilesh <akh.vyas@gmail.com>
2024-09-30 20:51:12 +0200

    module path

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
-from database import SessionLocal
-from model import SentimentRecord
+from .database import SessionLocal
+from .model import SentimentRecord
 
 
 # Model definition
